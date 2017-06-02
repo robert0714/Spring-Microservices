048cd436d609468271148be870721492683385bc
robert.lee <robert0714@gmail.com>
2017-06-02 18:18:40 +0800

    observing erake client

--- Chapter 5/chapter5.website/src/main/java/com/brownfield/pss/client/Application.java
+++ Chapter 5/chapter5.website/src/main/java/com/brownfield/pss/client/Application.java
@@ -17,73 +17,74 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient 
+@EnableDiscoveryClient
 public class Application implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	@Autowired
 	RestTemplate searchClient;
-	
+
 	@Autowired
 	RestTemplate bookingClient;
-	
+
 	@Autowired
 	RestTemplate checkInClient;
-	
-	//RestTemplate restClient= new RestTemplate();
-	
+
+	// RestTemplate restClient= new RestTemplate();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@Override
 	public void run(String... strings) throws Exception {
-		//Search for a flight
-		SearchQuery searchQuery = new SearchQuery("NYC","SFO","22-JAN-16");
- 		//Flight[] flights = searchClient.postForObject("http://search-service/search/get", searchQuery, Flight[].class); 
-		Flight[] flights = searchClient.postForObject("http://localhost:8095/api/search/get", searchQuery, Flight[].class); 
- 		
-		Arrays.asList(flights).forEach(flight -> logger.info(" flight >"+ flight));
-  		
-		//create a booking only if there are flights.
- 		if(flights == null || flights.length == 0){
- 			return;
- 		}
+		// Search for a flight
+		SearchQuery searchQuery = new SearchQuery("NYC", "SFO", "22-JAN-16");
+//		 Flight[] flights = searchClient.postForObject("http://search-service/search/get", searchQuery, Flight[].class);
+		Flight[] flights = searchClient.postForObject("http://search-apigateway/api/search/get", searchQuery,Flight[].class);
+
+		Arrays.asList(flights).forEach(flight -> logger.info(" flight >" + flight));
+
+		// create a booking only if there are flights.
+		if (flights == null || flights.length == 0) {
+			return;
+		}
 		Flight flight = flights[0];
-		BookingRecord booking = new BookingRecord(flight.getFlightNumber(),flight.getOrigin(),
-												  flight.getDestination(), flight.getFlightDate(),null,
-												  flight.getFares().getFare());
+		BookingRecord booking = new BookingRecord(flight.getFlightNumber(), flight.getOrigin(), flight.getDestination(),
+				flight.getFlightDate(), null, flight.getFares().getFare());
 		Set<Passenger> passengers = new HashSet<Passenger>();
-		passengers.add(new Passenger("Gavin","Franc","Male", booking));
+		passengers.add(new Passenger("Gavin", "Franc", "Male", booking));
 		booking.setPassengers(passengers);
-		long bookingId =0;
-		try { 
-			//long bookingId = bookingClient.postForObject("http://book-service/booking/create", booking, long.class); 
-			 bookingId = bookingClient.postForObject("http://book-apigateway/api/booking/create", booking, long.class); 
-			logger.info("Booking created "+ bookingId);
-		}catch (Exception e){
+		long bookingId = 0;
+		try {
+			// long bookingId = bookingClient.postForObject("http://book-service/booking/create", booking, long.class);
+			bookingId = bookingClient.postForObject("http://book-apigateway/api/booking/create", booking, long.class);
+			logger.info("Booking created " + bookingId);
+		} catch (Exception e) {
 			logger.error("BOOKING SERVICE NOT AVAILABLE...!!!");
 		}
-		
-		//check in passenger
-		if(bookingId == 0) return;
+
+		// check in passenger
+		if (bookingId == 0)
+			return;
 		try {
-			CheckInRecord checkIn = new CheckInRecord("Franc", "Gavin", "28C", null, "BF101","22-JAN-16", bookingId);
-			long checkinId = checkInClient.postForObject("http://checkin-apigateway/api/checkin/create", checkIn, long.class); 
-			logger.info("Checked IN "+ checkinId);
-		}catch (Exception e){
-			logger.error("CHECK IN SERVICE NOT AVAILABLE...!!!"); 
+			CheckInRecord checkIn = new CheckInRecord("Franc", "Gavin", "28C", null, "BF101", "22-JAN-16", bookingId);
+			long checkinId = checkInClient.postForObject("http://checkin-apigateway/api/checkin/create", checkIn,
+					long.class);
+			logger.info("Checked IN " + checkinId);
+		} catch (Exception e) {
+			logger.error("CHECK IN SERVICE NOT AVAILABLE...!!!");
 		}
 	}
-		
+
 }
 
 @Configuration
 class AppConfiguration {
 
-    @LoadBalanced
-    @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
