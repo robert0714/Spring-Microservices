ed7a79b1046edc01f0a44bda8c98db2b6e9dc0fa
robert.lee <robert0714@gmail.com>
2017-08-02 17:27:15 +0800

    update version

--- Chapter 6/chapter6.search-apigateway/src/test/java/com/example/ApplicationTests.java
+++ Chapter 6/chapter6.search-apigateway/src/test/java/com/example/ApplicationTests.java
@@ -1,14 +1,12 @@
 package com.example;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.junit.runner.RunWith; 
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.brownfield.pss.search.apigateway.SearchApiGateway;
-
+ 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SearchApiGateway.class)
+@SpringBootTest 
 public class ApplicationTests {
 
 	@Test
